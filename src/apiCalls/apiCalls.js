7d8b935b9300fa6ef951bd7c49829c306c8cca90
Austin.Wiedenman <awiedenman@gmail.com>
2018-05-24 15:11:49 -0600

    Add tests for searchDataFetch

---
+++ src/apiCalls/apiCalls.js
@@ -0,0 +1,8 @@
+
+export const searchDataCall = async ( condition ) => {
+  const url = `https://api.opentrials.net/v1/search?q=${ condition }&page=1&per_page=100`;
+  const rawOpenTrailsData = await fetch(url);
+  const openTrailsData = await rawOpenTrailsData.json();
+  return openTrailsData;
+  console.log('open trails', openTrailsData )
+}
---
+++ src/apiCalls/apiCalls.test.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { searchDataCall } from './apiCalls';
+
+
+describe('apiCalls', () => {
+  const mockData = {
+    "id": "cabb91cc-46b9-11e7-ba62-0242ac12000d",
+    "source_id": "isrctn",
+    "identifiers": {
+      "isrctn": "ISRCTN15621797"
+    }
+  }
+
+  describe('searchDataCall', () => {
+
+    it('returns the correct informaiton', async() => {
+      window.fetch = jest.fn().mockImplementation( () => Promise.resolve({
+        status: 200,
+        json: () => Promise.resolve( mockData )
+      }))
+      const result = await searchDataCall('breast cancer');
+      const expected = mockData;
+
+      expect(result).toEqual(expected);
+    })
+
+    it('calls the fetch with the correct parametrs', () => {
+      window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
+        status: 200,
+        json: () => Promise.resolve(mockData)
+      }))
+      
+      const expected = `https://api.opentrials.net/v1/search?q=breast cancer&page=1&per_page=100`;
+
+      searchDataCall('breast cancer');
+
+      expect(window.fetch).toHaveBeenCalledWith(expected);
+    })
+
+    it('returns an error if the sttus is bad', async() => {
+      window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
+        status: 500
+      }))
+      const expected = Error('status failure: 500');
+      const result = await searchDataCall('breast cancer');
+
+      expect(result).rejects.toEqual(expected);
+
+    })
+
+  })
+
+
+
+})
