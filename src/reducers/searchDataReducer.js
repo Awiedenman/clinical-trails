08aacc7f124cb729e7cdb78b9058a321859f9e6a
Austin.Wiedenman <awiedenman@gmail.com>
2018-05-25 13:18:23 -0600

    Update searchDataReducer to populate store

--- src/reducers/searchDataReducer.js
+++ src/reducers/searchDataReducer.js
@@ -1,12 +1,11 @@
 export const searchDataReducer = ( state = [], action ) => {
   switch (action.type) {
     case 'POPULATE_SEARCH_DATA':
-     return [ ...action.searchData ]
+    console.log(action.searchData)
+     return action.searchData.items
   
     default: 
       return state
   }
+}
 
-  }
-
-  export default searchDataReducer;
