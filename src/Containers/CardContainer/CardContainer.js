3a11aa233d6257f3ae98af5356047f10d4d4e62a
Austin.Wiedenman <awiedenman@gmail.com>
2018-05-28 09:08:22 -0600

    Render trial information on page

--- src/Containers/CardContainer/CardContainer.js
+++ src/Containers/CardContainer/CardContainer.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import { connect } from 'react-redux';
-// import { Card } from '../../Components/Card';
+import { Card } from '../../Containers/Card/Card';
 
-const CardContainer =( props ) => {
-
+const CardContainer =({ searchData }) => {
+  const createCards = searchData.map(( trial,index) => {
+    return (
+      <Card 
+      { ...trial }
+      key={index}
+      />
+    )
+  })
 
   return(
-    <div></div>
+    <div>{ createCards }</div>
   )
 }
 
