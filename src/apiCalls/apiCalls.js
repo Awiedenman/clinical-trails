5eab8b7b147635338e52c0144e5a1a706cf95bba
Austin.Wiedenman <awiedenman@gmail.com>
2018-05-28 08:00:58 -0600

    Add try catch to apiCall.js

--- src/apiCalls/apiCalls.js
+++ src/apiCalls/apiCalls.js
@@ -1,8 +1,16 @@
-
-export const searchDataCall = async ( condition ) => {
+export const searchDataCall = async (condition) => {
   const url = `https://api.opentrials.net/v1/search?q=${ condition }&page=1&per_page=100`;
-  const rawOpenTrailsData = await fetch(url);
-  const openTrailsData = await rawOpenTrailsData.json();
-  console.log('open trails', openTrailsData )
-  return openTrailsData;
+  // https: //api.opentrials.net/v1/search?q=arthritis&page=1&per_page=100
+  try {
+    const rawOpenTrailsData = await fetch(url);
+    if (rawOpenTrailsData.status === 200) {
+      const openTrailsData = await rawOpenTrailsData.json();
+      console.log('open trails', openTrailsData)
+      return openTrailsData;
+    } else {
+      throw Error(`Status failure: ${rawOpenTrailsData.status}`)
+    }
+  } catch (error) {
+    throw Error;
+  }
 }
