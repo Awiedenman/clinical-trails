b47a487817af101d989ae98b99aa8c8ffb780915
Austin.Wiedenman <awiedenman@gmail.com>
2018-05-28 10:04:33 -0600

    Build controlled form for LogIn

--- src/Containers/LogIn/Login.js
+++ src/Containers/LogIn/Login.js
@@ -1,1 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
+
+class LogIn extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      email: '',
+      password: ''
+    }
+  }
+
+  render(){
+    return(
+      <div>
+        
+      </div>
+    )
+  }
+}
